45b24b99a19a1d348b979e7eaed2e7f827c69b0c
podestal <luisrodriguezpodesta@gmail.com>
2023-11-02 15:31:43 -0400

    random walk

--- hirst-painting/playground.py
+++ hirst-painting/playground.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 my_turtle = Turtle()
 my_turtle.color('blue')
@@ -19,21 +20,29 @@ my_turtle.color('blue')
 
 ############## Drawing different shapes ###########
 
-n_lines = 3
-distance = 100
+# n_lines = 3
+# distance = 100
 
-while n_lines < 8:
+# while n_lines < 8:
 
-    angle = int(360 / n_lines)
-    for _ in range(n_lines):
-        my_turtle.forward(100)
-        my_turtle.right(angle)
-    n_lines += 1
-    distance += 20
+#     angle = int(360 / n_lines)
+#     for _ in range(n_lines):
+#         my_turtle.forward(100)
+#         my_turtle.right(angle)
+#     n_lines += 1
+#     distance += 20
 
+############### Random Walk #############
 
+colors = ['CornflowerBlue', 'DarkOrchid', 'IndianRed', 'DeepSkyBlue', 'LightSeaGreen', 'wheat', 'SlateGray', "SeaGreen"]
+directions = [0, 90, 180, 270]
+my_turtle.pensize(15)
+my_turtle.speed('fastest')
 
-
+for _ in range(400):
+    my_turtle.color(random.choice(colors))
+    my_turtle.forward(30)
+    my_turtle.setheading(random.choice(directions))
 
 
 
