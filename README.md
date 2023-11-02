c8f8ba939c96e8e91040fc922754d13a82bed3ca
podestal <luisrodriguezpodesta@gmail.com>
2023-11-02 14:15:32 -0400

    draw square

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # python-intermediate-projects
 This repostitory is going to hold a list of intermediate python prjects, each project is going to be specific for a concept in python:
 1. Quizz game -> OOP
+2. Hirst game -> GUI
---
+++ hirst-painting/playground.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen
+
+# my_turtle.color('red')
+# my_turtle.forward(100)
+# my_turtle.right(90)
+
+############## Drawing a square ##########
+
+my_turtle = Turtle()
+my_turtle.color('blue')
+for _ in range(4):
+    my_turtle.forward(200)
+    my_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
