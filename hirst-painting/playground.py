35be23f6b5dd1808f2677db5a9d30454b027822b
podestal <luisrodriguezpodesta@gmail.com>
2023-11-02 14:33:53 -0400

    dash line

--- hirst-painting/playground.py
+++ hirst-painting/playground.py
@@ -1,16 +1,21 @@
 from turtle import Turtle, Screen
 
-# my_turtle.color('red')
-# my_turtle.forward(100)
-# my_turtle.right(90)
+my_turtle = Turtle()
+my_turtle.color('blue')
 
 ############## Drawing a square ##########
 
-my_turtle = Turtle()
-my_turtle.color('blue')
-for _ in range(4):
-    my_turtle.forward(200)
-    my_turtle.right(90)
+# for _ in range(4):
+#     my_turtle.forward(200)
+#     my_turtle.right(90)
+
+############## Dash line ###################
+
+for _ in range(15):
+    my_turtle.forward(10)
+    my_turtle.penup()
+    my_turtle.forward(10)
+    my_turtle.pendown()
 
 
 
