494b286c5a395f9d9c79c897de84fd56cdc0d6db
podestal <luisrodriguezpodesta@gmail.com>
2023-11-05 14:31:07 -0500

    turtle race

--- turtle-race/race.py
+++ turtle-race/race.py
@@ -1,26 +1,67 @@
+# from turtle import Turtle, Screen
+
+# t = Turtle()
+# screen = Screen()
+
+# def move_forward():
+#     t.forward(10)
+
+# def move_backwards():
+#     t.backward(10)
+
+# def turn_left():
+#     new_heading = t.heading() + 10
+#     t.setheading(new_heading)
+
+# def turn_right():
+#     new_heading = t.heading() - 10
+#     t.setheading(new_heading)
+
+# def clear():
+#     t.clear()
+#     t.penup()
+#     t.home()
+#     t.pendown()
+
+# screen.listen()
+# screen.onkey(move_forward, 'w')
+# screen.onkey(move_backwards, 's')
+# screen.onkey(turn_left, 'a')
+# screen.onkey(turn_right, 'd')
+# screen.onkey(clear, 'c')
+# screen.exitonclick()
+
 from turtle import Turtle, Screen
+import random
 
-t = Turtle()
+
 screen = Screen()
-t.pensize(10)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title='Make your bet', prompt='Which turtle will win the race? Enter a color: ')
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+list_of_turtles = []
+race_on = True
 
-def move_forward():
-    t.forward(10)
+y_axis = -90
 
-def move_backwards():
-    t.backward(10)
+for color in colors:
+    new_turtle = Turtle(shape='turtle')
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=-y_axis)
+    new_turtle.color(color)
+    y_axis += 30
+    list_of_turtles.append(new_turtle)
 
-def turn_left():
-    new_heading = t.heading() + 10
-    t.setheading(new_heading)
+while race_on:
+    for turtle in list_of_turtles:
+        if turtle.xcor() > 230:
+            race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print('you win')
+            else:
+                print('you lose')
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
 
-def turn_right():
-    new_heading = t.heading() - 10
-    t.setheading(new_heading)
-
-screen.listen()
-screen.onkey(move_forward, 'w')
-screen.onkey(move_backwards, 's')
-screen.onkey(turn_left, 'a')
-screen.onkey(turn_right, 'd')
 screen.exitonclick()
