188d2ad7445bb1d76c56eab8cae79bee9d6d6b26
podestal <luisrodriguezpodesta@gmail.com>
2023-11-11 20:17:54 -0500

    list comprehension

---
+++ NATO-alphabet/playground.py
@@ -0,0 +1,29 @@
+numbers = [1, 2, 3]
+# new_list = []
+# for n in numbers:
+#     add_1 = n + 1
+#     new_list.append(add_1)
+
+####### List Comprehension #########
+# new_list = [item + 1 for item in numbers]
+# name = 'Luis'
+# letters_list = [letter for letter in name]
+# print(letters_list)
+# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+# short_names = [name for name in names if len(name) < 5]
+# print(short_names)
+# upper_names = [name.upper() for name in names]
+# print(upper_names)
+
+####### Squaring Numbers #####
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# squared_numbers = [num * num for num in numbers]
+# print(squared_numbers)
+
+####### Filtering Even Numbers #######
+numbers = [9, 0, 32, 8, 2, 8, 64, 29, 42, 99]
+even_numbers = [number for number in numbers if number % 2 == 0]
+print(even_numbers)
+
+####### Data Overlap ##############
+
