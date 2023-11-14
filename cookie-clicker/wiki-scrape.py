fe67cd63afb5f3a43ca84b160c8e99a1f9b965a6
podestal <luisrodriguezpodesta@gmail.com>
2023-11-14 12:20:58 -0500

    wiki navigating

--- cookie-clicker/wiki-scrape.py
+++ cookie-clicker/wiki-scrape.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
@@ -7,7 +9,17 @@ chrome_options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://en.wikipedia.org/wiki/Main_Page')
 
-interaccions = driver.find_element(By.CSS_SELECTOR, value='div#articlecount a')
-print(interaccions.text)
+driver.maximize_window()
 
-driver.quit()
+interaccions = driver.find_element(By.CSS_SELECTOR, value='div#articlecount a')
+# interaccions.click()
+
+view_source = driver.find_element(By.PARTIAL_LINK_TEXT, value='View source')
+# view_source.click()
+
+search_bar = driver.find_element(By.NAME, value='search')
+search_bar.send_keys('Python')
+search_bar1 = driver.find_element(By.NAME, "search")
+search_bar1.send_keys(Keys.ENTER)
+
+# driver.quit()
