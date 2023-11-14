55bd8da56011d0c3f83527ea28d6503aa633a92a
podestal <luisrodriguezpodesta@gmail.com>
2023-11-14 11:31:38 -0500

    wiki scrape

---
+++ cookie-clicker/wiki-scrape.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get('https://en.wikipedia.org/wiki/Main_Page')
+
+interaccions = driver.find_element(By.CSS_SELECTOR, value='div#articlecount a')
+print(interaccions.text)
+
+driver.quit()
