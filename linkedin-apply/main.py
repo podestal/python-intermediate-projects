cd8f7fb1692493729ca0f329bdd33608ef45a05d
podestal <luisrodriguezpodesta@gmail.com>
2023-11-15 11:34:27 -0500

    apply to a job

--- linkedin-apply/main.py
+++ linkedin-apply/main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from chrome_options import ChromeOptions
 import time
 
 chrome_options = ChromeOptions()
 driver = webdriver.Chrome(options=chrome_options.get_options())
+driver.maximize_window()
 
 # Login Automatically
 driver.get('https://www.linkedin.com')
@@ -19,6 +21,14 @@ signin_button = driver.find_element(By.XPATH, value='//*[@id="main-content"]/sec
 signin_button.click()
 
 # Apply for a Job
-# Apply for all the jobs
+driver.get('https://www.linkedin.com/jobs/search/?currentJobId=3740965769&f_AL=true&keywords=Data%20entry%20remote&origin=JOB_SEARCH_PAGE_JOB_FILTER')
+time.sleep(5)
+apply_button = driver.find_element(By.CSS_SELECTOR, value='div.jobs-apply-button--top-card button')
+print(apply_button.text)
+apply_button.click()
 
-# driver.quit()
+time.sleep(5)
+send = driver.find_element(By.CSS_SELECTOR, value='div button.artdeco-button')
+send.click()
+
+driver.quit()
